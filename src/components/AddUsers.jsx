a1d07bc99081bc50ff0ba47509d908a42598c62b
anuraggsps <ankit@softprodigy.com>
2024-03-31 21:57:52 +0530

    validation addded

--- src/components/AddUsers.jsx
+++ src/components/AddUsers.jsx
@@ -9,7 +9,7 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import {addUser} from '../service/api.js'
+import { addUser } from "../service/api.js";
 
 const Container = styled(FormGroup)`
   width: 50%;
@@ -27,37 +27,55 @@ const initialValue = {
 };
 
 const AddUsers = () => {
-  const [user, setUser ] = useState(initialValue);
+  const [user, setUser] = useState(initialValue);
+  const [error, setError] = useState([]);
   const navigate = useNavigate();
   const onChangeValue = (value) => {
-    setUser({...user,[value.target.name]:value.target.value})
-  
+    setUser({ ...user, [value.target.name]: value.target.value });
   };
-  const userDetails = async()=>{
+  const userDetails = async () => {
+    if (!user.firstName || user.firstName.length === 0){
+      setError({ firstName: "first name blank" });
+      return false;
+      
+    }
+      
     await addUser(user);
-    navigate('/all-users');
-  }
+    
+    navigate("/all-users");
+  };
   return (
     <Container>
       <Typography variant="h3">Add User</Typography>
       <FormControl>
         <InputLabel>First Name</InputLabel>
-        <Input onChange={(e) => onChangeValue(e)} name="firstName"/>
+        <Input
+          onChange={(e) => onChangeValue(e)}
+          name="firstName"
+          error={!!error.firstName}
+        />
+        {error.firstName && (
+          <Typography variant="caption" color="error">
+            {error.firstName}
+          </Typography>
+        )}
       </FormControl>
       <FormControl>
         <InputLabel>Last Name</InputLabel>
-        <Input onChange={(e) => onChangeValue(e)} name="lastName"/>
+        <Input onChange={(e) => onChangeValue(e)} name="lastName" />
       </FormControl>
       <FormControl>
         <InputLabel>Mobile Number</InputLabel>
-        <Input onChange={(e) => onChangeValue(e)} name="mobileNumber"/>
+        <Input onChange={(e) => onChangeValue(e)} name="mobileNumber" />
       </FormControl>
       <FormControl>
         <InputLabel>Email ID</InputLabel>
-        <Input onChange={(e) => onChangeValue(e)} name="email"/>
+        <Input onChange={(e) => onChangeValue(e)} name="email" />
       </FormControl>
       <FormControl>
-        <Button onClick={()=>userDetails()} variant="contained">Submit</Button>
+        <Button onClick={() => userDetails()} variant="contained">
+          Submit
+        </Button>
       </FormControl>
     </Container>
   );
